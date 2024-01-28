3fe0d5de9d8f9f61d7b49279236a910f59e4fd8d
katorivn699 <78785812+katorivn699@users.noreply.github.com>
2024-01-28 23:40:16 +0700

    Update date
    May be

--- script/myscript.js
+++ script/myscript.js
@@ -6,10 +6,25 @@ function startTime() {
     m = checkTime(m);
     s = checkTime(s);
     document.getElementById('txt').innerHTML =
-    h + ":" + m + ":" + s;
+        h + ":" + m + ":" + s;
     var t = setTimeout(startTime, 500);
 }
+
+function startDate() {
+    var today = new Date();
+    var d = today.getDate();
+    var month = today.getMonth() + 1;
+    var y = today.getFullYear();
+    d = checkDate(d);
+    document.getElementById('date').innerHTML = "Date: " + d + "/" + month + "/" + y;
+    var t = setTimeout(startDate, 10000)
+}
+
+function checkDate(i) {
+    if (i < 10) { i = "0" + i };
+    return i;
+}
 function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
     return i;
 }
